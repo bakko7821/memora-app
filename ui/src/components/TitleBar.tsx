4cf23353c7f27da046777137a52ff432e2c5296f
Dmitry Eshman <bakko7821@gmail.com>
2026-04-05 23:44:43 +0300

    style: add svg from buttons TitleBar

--- ui/src/components/TitleBar.tsx
+++ ui/src/components/TitleBar.tsx
@@ -1,3 +1,7 @@
+import CrossIcon from "../assets/icons/CrossFilled.svg?react"
+import MinimizeIcon from "../assets/icons/Minus.svg?react"
+import MaximizeIcon from "../assets/icons/Maximize2.svg?react"
+
 export const TitleBar = () => {
     return (
         <header className="w-full bg-gray-500 p-0 flex flex-row items-center justify-between">
@@ -6,9 +10,15 @@ export const TitleBar = () => {
                 <h1 className="text-base font-semibold text-pink-300">Memora</h1>
             </div>
             <div className="flex flex-row gap-0">
-                <button onClick={() => window.electronAPI.minimize()} className="w-8 h-8 bg-amber-50"></button>
-                <button onClick={() => window.electronAPI.maximizeToggle()} className="w-8 h-8 bg-red-300"></button>
-                <button onClick={() => window.electronAPI.close()} className="w-8 h-8 bg-green-300"></button>
+                <button onClick={() => window.electronAPI.minimize()} className="flex items-center justify-center p-2 bg-red-300 hover:bg-gray-500">
+                    <MinimizeIcon width={24} height={24} color="#ffffff"/>
+                </button>
+                <button onClick={() => window.electronAPI.maximizeToggle()} className="flex items-center justify-center p-2 bg-red-300 hover:bg-gray-500">
+                    <MaximizeIcon width={24} height={24} color="#ffffff"/>
+                </button>
+                <button onClick={() => window.electronAPI.close()} className="flex items-center justify-center p-2 bg-red-300 hover:bg-gray-500">
+                    <CrossIcon width={24} height={24} color="#ffffff"/>
+                </button>
             </div>
         </header>
     )
